fc53f98db451ae6fd98da490c28f85bf41b58a94
Charly <100117263+CharlyAnne@users.noreply.github.com>
2023-05-05 11:42:06 +0100

    Still fixing the request status function

--- web_dynamic/static/scripts/2-hbnb.js
+++ web_dynamic/static/scripts/2-hbnb.js
@@ -20,18 +20,18 @@ $(function () {
   });
   
   //Requesting api status
-  const statusMode = $('DIV#api_status');
+  const stat = $('div#api_status');
   
   $.ajax({
     type: 'GET',
     url: 'http://127.0.0.1:5001/api/v1/status/',
-    success: (data, statusMsg, jqXHR) => {
+    success: (data, textStatus, jqXHR) => {
       if (jqXHR.status === "OK") {
-        statusMode.addClass('available');
+        stat.addClass('available');
       }
     },
     error: () => {
-      statusMode.removeClass('available');
+      stat.removeClass('available');
     }
   });
 });
